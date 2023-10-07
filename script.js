59ffd4f6f787867d9b39ea115846c622f5728021
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2023-10-07 14:59:32 -0500

    body will remove menuMobile once clicked
    body will hidde menu Mobile once Clicked

--- script.js
+++ script.js
@@ -15,9 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = d.querySelector('.nav__icon')
    const menuMobile = d.querySelector('.mobileMenu')
    const closeIcon = d.querySelector('.mobileMenu__closeIcon')
+   const body = d.body
 
    menuIcon.addEventListener('click', showMobileMenu)
    closeIcon.addEventListener('click',closeMobileMenu)
+
+   
+   
 
    function showMobileMenu(){
     menuMobile.classList.toggle('inactive')
@@ -26,10 +30,23 @@ document.addEventListener("DOMContentLoaded", function () {
    function closeMobileMenu(){
     if(!menuMobile.classList.contains('inactive')){
         menuMobile.classList.add('inactive')
+        
     }
 
+
    }
- 
+
+   body.addEventListener('click', function (event) {
+    
+    // Verificar si el menú no está inactivo y si el clic no ocurrió en el menú o en el icono del menú
+    if (!menuMobile.classList.contains('inactive') && !menuIcon.contains(event.target) && !menuMobile.contains(event.target)) {
+        
+        // Aquí puedes agregar el código que deseas ejecutar cuando el menú está abierto y se hace clic fuera de él.
+        // Por ejemplo, puedes cerrar el menú aquí.
+        menuMobile.classList.add('inactive');
+    }
+});
+  
     
 });
 
