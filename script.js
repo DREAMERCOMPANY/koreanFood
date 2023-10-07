e328ede8b264855dba1c0dade3cefd29c44a5537
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2023-10-07 14:38:28 -0500

    Added mobile menu

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-    const loader = document.querySelector(".loader");
-    const mainContainer = document.querySelector(".main-container");
+    const d = document;
+
+     const loader = d.querySelector(".loader");
+    const mainContainer = d.querySelector(".main-container");
+
 
     // Escucha el evento 'animationend' en el loader
     loader.addEventListener("animationend", function () {
-        mainContainer.style.height = "auto";
-       
-    });
+        mainContainer.style.height = 'auto'
+        
+    }); 
 
-   
+    const menuIcon = d.querySelector('.nav__icon')
+   const menuMobile = d.querySelector('.mobileMenu')
+   const closeIcon = d.querySelector('.mobileMenu__closeIcon')
+
+   menuIcon.addEventListener('click', showMobileMenu)
+   closeIcon.addEventListener('click',closeMobileMenu)
+
+   function showMobileMenu(){
+    menuMobile.classList.toggle('inactive')
+   }
+
+   function closeMobileMenu(){
+    if(!menuMobile.classList.contains('inactive')){
+        menuMobile.classList.add('inactive')
+    }
+
+   }
+ 
+    
 });
+
+
